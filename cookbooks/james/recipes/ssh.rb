a760a09c909206ce52e902a2255b972382dc374e
James Bartlett <jmabartlett2@gmail.com>
2023-10-11 13:10:26 -0700

    add ssh config

--- cookbooks/james/recipes/ssh.rb
+++ cookbooks/james/recipes/ssh.rb
@@ -5,3 +5,11 @@ cookbook_file "#{node['home_dir']}/bin/ssh_tmux" do
   source 'ssh_tmux'
   action :create
 end
+
+cookbook_file "#{node['home_dir']}/.ssh/config" do
+  owner node['owner']
+  group node['group']
+  mode '0755'
+  source 'ssh_config'
+  action :create
+end
