f1a862d2e3c6bb895673beadd5735596234d4ff5
James Bartlett <jmabartlett2@gmail.com>
2023-10-18 10:49:53 -0700

    Add tmux-resurrect

--- cookbooks/james/recipes/tmux.rb
+++ cookbooks/james/recipes/tmux.rb
@@ -4,3 +4,20 @@ cookbook_file "#{node['home_dir']}/.tmux.conf" do
   mode '0644'
   source 'tmux.conf'
 end
+
+directory "#{node['home_dir']}/.tmux/plugins" do
+  owner node['owner']
+  group node['group']
+  mode '0755'
+  action :create
+  recursive true
+end
+
+git 'TPM' do
+  repository 'https://github.com/tmux-plugins/tpm.git'
+  revision 'v3.1.0'
+  destination "#{node['home_dir']}/.tmux/plugins/tpm"
+  user node['owner']
+  group node['group']
+  action :sync
+end
