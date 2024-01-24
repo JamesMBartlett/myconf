6c18b054296f72463df2740eba325989b87ea205
James Bartlett <jmabartlett2@gmail.com>
2024-01-24 14:44:41 -0800

    Add .ssh/rc

--- cookbooks/james/recipes/ssh.rb
+++ cookbooks/james/recipes/ssh.rb
@@ -13,3 +13,11 @@ cookbook_file "#{node['home_dir']}/.ssh/config" do
   source 'ssh_config'
   action :create
 end
+
+cookbook_file "#{node['home_dir']}/.ssh/rc" do
+  owner node['owner']
+  group node['group']
+  mode '0755'
+  source 'ssh_rc'
+  action :create
+end
